6495dc8ef366db19e431954f4b6bd3cff1fc7f35
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-15 16:49:33 +0530

    redwood: Import fingerprint sensors keylayouts
    Signed-off-by: Choirul Umam <bavvtm@outlook.com>

--- device.mk
+++ device.mk
@@ -244,7 +244,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/configs/keylayout/lahaina-yupikqrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/lahaina-yupikqrd-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/configs/keylayout/lahaina-yupikqrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/lahaina-yupikqrd-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
 # Media
 PRODUCT_PACKAGES += \
