e7435849c299f85d63007e64d5f9b3bbc900ae40
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-12-26 22:34:11 +0530

    redwood: Link some libs against v33 libstagefright_foundation
     * fixes crash and/or high CPU usage
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Choirul Umam <bavvtm@outlook.com>

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,9 @@ function blob_fixup() {
         vendor/lib64/hw/camera.qcom.so)
             sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
             ;;
+        vendor/lib64/hw/displayfeature.default.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
