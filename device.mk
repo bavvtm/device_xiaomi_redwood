571cb28810be0597a5fa7df54569adcdd1efc76f
Alexander Koskovich <akoskovich@pm.me>
2024-02-01 00:37:26 +0700

    redwood: Update to Boot Control HAL v1.2

--- device.mk
+++ device.mk
@@ -21,9 +21,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service \
     checkpoint_gc \
     otapreopt_script \
     update_engine \
